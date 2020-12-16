351a88080b905d3cfc06710475fecf3d76931ff7
JPDev <jotepauder01@gmail.com>
2020-12-15 22:41:15 -0300

    exemplo primeiro

---
+++ ViradoNoJiraya.java
@@ -0,0 +1,6 @@
+public class ViradoNoJiraya {
+    public static void main(String[] args){
+        System.out.println("Meu deus, sou um hacker");
+        System.out.println("Utilizando no vscode");
+    }
+}
