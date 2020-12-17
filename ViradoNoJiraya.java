79a1c4f8bd7861184148d28c5cbac86e30d91276
JPDev <jotepauder01@gmail.com>
2020-12-16 22:16:02 -0300

    update dicas and first exemple java

--- ViradoNoJiraya.java
+++ ViradoNoJiraya.java
@@ -1,6 +1,6 @@
-public class ViradoNoJiraya {
-    public static void main(String[] args){
-        System.out.println("Meu deus, sou um hacker");
-        System.out.println("Utilizando no vscode");
+public class ViradoNoJiraya { // Classe
+    public static void main(String[] args){ // Método
+        int idade = 29; // Variavel 
+        System.out.println("A minha idade é: " + idade + " anos"); // Imprimir o valor ( comando de saida) - Concatenaçao String e valor na memoria 
     }
 }
