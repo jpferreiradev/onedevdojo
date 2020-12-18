a9f1d38585b222426f6624fb99817f0f5949f980
JPDev <jotepauder01@gmail.com>
2020-12-18 20:09:37 -0300

    update dicas and add class intellj

---
+++ logica/src/CalculadorDiferencaIdade.java
@@ -0,0 +1,11 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 29;
+        int secondAge = 28;
+        int ageDifference = firstAge - secondAge;
+
+        System.out.println("A diferença das duas idades é de " + ageDifference + " anos");
+        System.out.println("Bora volta caraio! ");
+
+    }
+}
