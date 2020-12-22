6f0762020f82e1331827d30c600bf5899bac4e12
JPDev <jotepauder01@gmail.com>
2020-12-22 19:50:51 -0300

    update dicas

--- logica/src/CalculadoraImposto.java
+++ logica/src/CalculadoraImposto.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class CalculadoraImposto {
     public static void main(String[] args){
-     int
-     System.out.println("");
+     int calImposto, n;
+     double valorSalario;
 
+     System.out.println("Qual é o valor do salário?");
+     valorSalario = ler.nextDouble();
 
+     //Scanner ler = new Scanner(System.in);
+     //System.out.println("O valor do salario com o imposto é de: ");
+     //Scanner ler = new Scanner(System.in);
 
 
     }
